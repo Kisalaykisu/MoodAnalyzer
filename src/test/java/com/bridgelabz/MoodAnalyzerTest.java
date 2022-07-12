1af4444f70f42067c138360a5e689222f51949b4
Kisalay <kisalaykisu@gmail.com>
2022-07-13 00:46:58 +0530

    UC2 has been added

--- src/test/java/com/bridgelabz/MoodAnalyzerTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyzerTest.java
@@ -4,18 +4,25 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MoodAnalyzerTest {
-    @Test
-    public void testMoodAnalysis_whenMoodIsSad() { //TC 1.1 "I am in sad mood"
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-        String mood = moodAnalyzer.analyseMood("I am in sad mood");
-        Assertions.assertEquals(mood, "SAD");
+
+    public void testMoodAnalysis_whenMoodIsSad() { //tc 1.1 Iamin sad mood
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("This is a sad message");
+        String mood = moodAnalyzer.analyseMood();
+        Assertions.assertEquals(mood,"SAD");
     }
 
     @Test
-    public void testMoodAnalysis_whenMoodIsHappy() { //TC 1.2 I am in happy mood
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-        String mood1 = moodAnalyzer.analyseMood("I am in happy mood");
-        Assertions.assertEquals(mood1, "HAPPY");
+    public void testMoodAnalysis_whenMoodIsHappy() {//tc 1.2 Iamin sad mood
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("This is a happy message");
+        String mood1 = moodAnalyzer.analyseMood();
+        Assertions.assertEquals(mood1,"HAPPY");
+
+    }
+    @Test
+    public void testMoodAnalysis_whenMoodIsNull() { //tc 2.1
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer(null);
+        String mood2 = moodAnalyzer.analyseMood();
+        Assertions.assertEquals(mood2,"HAPPY");
+
     }
 }
-
